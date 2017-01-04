7977b1917218920d92d7b55026a97f10109b46b8
JonSn0w <therealbrettstevenson@gmail.com>
2017-01-04 00:16:40 -0800

    Add modular size functionalities

--- @Resources/Config.lua
+++ @Resources/Config.lua
@@ -2,20 +2,18 @@
 -- Filename: config.lua
 -- Author: Brett Stevenson
 -- Project: GitHubCalendar
--- License: BSD 3-Clause 
+-- License: BSD 3-Clause
 -- Updated: July 31, 2016
--- ###############################################################################
-
--- ## Description ###################################################################
--- A work-in-progress Lua script, intended to improve the ease of use of the GitHubCalendar Rainmeter 
--- skin by automating much of the user configuration processes. 
--- ###############################################################################
+-- Description: A work-in-progress Lua script, intended to improve the ease of
+-- use of the GitHubCalendar Rainmeter skin by automating much of the user
+-- configuration processes. ###############################################################################
 
 
 function Initialize()
 	path = SELF:GetOption('WriteTo')
 	Update()
 end
+
 
 function Update()
 	local file = io.open(path, "r")
@@ -25,11 +23,13 @@ function Update()
 	return tostring(data)
 end
 
+
 function Write()
 	local file = io.open(path, "w")
 	local username = SELF:GetOption('Input')
+	local numWeeks = SELF:GetOption('Weeks')
 	io.output(file)
 	io.write(tostring(username))
+	io.write(tostring(numWeeks))
 	io.close(file)
-	return tostring(username)
 end
